e4d378706fb8ec75201b28416f2af2f927711853
averangeall <averangeall@gmail.com>
2013-07-20 21:20:03 +0800

    Fix download empty problem

--- download.sh
+++ download.sh
@@ -2,21 +2,30 @@ if [[ "$#" != "2" ]]
 then
     echo 'usage:'
     echo '    bash download.sh gag_id image_url'
-    exit
+    exit 1
 fi
 
 gag_id=$1
 image_url=$2
 
 rm -f images/$gag_id.*
-wget -q -O images/$gag_id $image_url 2>&1 > /dev/null
-desc=`file images/$gag_id`
-if [[ "$desc" =~ "GIF" ]]
-then
-    convert images/$gag_id[0] images/$gag_id.jpg
-    rm -f images/$gag_id
-elif [[ "$desc" =~ "JPEG" ]]
-then
-    mv images/$gag_id images/$gag_id.jpg
-fi
+okay=false
+while [[ "$okay" == "false" ]]
+do
+    wget -q -O "images/$gag_id" "$image_url"
+    desc=`file images/$gag_id`
+    if [[ "$desc" =~ "GIF" ]]
+    then
+        convert images/$gag_id[0] images/$gag_id.jpg
+        rm -f images/$gag_id
+        okay=true
+    elif [[ "$desc" =~ "JPEG" ]]
+    then
+        mv images/$gag_id images/$gag_id.jpg
+        okay=true
+    fi
+    sleep 1
+done
+
+exit 0
 
