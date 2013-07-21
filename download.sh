c4b0ea57a4dbf35aece58e982c4a9cb11433fd2b
averangeall <averangeall@gmail.com>
2013-07-21 13:27:49 +0800

    Don't wget if file already exists

--- download.sh
+++ download.sh
@@ -12,6 +12,10 @@ rm -f images/$gag_id.*
 okay=false
 while [[ "$okay" == "false" ]]
 do
+    if [ -e images/$gag_id.jpg ]
+    then
+        break
+    fi
     wget -q -O "images/$gag_id" "$image_url"
     desc=`file images/$gag_id`
     if [[ "$desc" =~ "GIF" ]]
